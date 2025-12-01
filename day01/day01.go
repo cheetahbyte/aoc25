fabe09ea5ac6e4a3eaebde80d6e65cb64e23bf14
Leonhard Breuer <git@leonhardbreuer.de>
2025-12-01 07:09:59 +0100

    final optimization day 01

--- day01/day01.go
+++ day01/day01.go
@@ -5,74 +5,97 @@ import (
 	"github.com/cheetahbyte/aoc25/util"
 )
 
+const (
+	dialSize       = 100
+	startPos       = 50
+	directionRight = 82
+)
+
 func mod(a, b int) int {
 	return ((a % b) + b) % b
 }
 
-func Part1() any {
+type instruction struct {
+	right bool
+	steps int
+}
+
+var instructions []instruction
+
+func parseInstructions() []instruction {
 	data := util.GetData()
-	var currentPosition = 50
-	var zeroCounter = 0
-	for _, v := range *data {
-		direction := v[0]
-		var steps = 0
+	result := make([]instruction, len(*data))
+
+	for i, v := range *data {
+		right := v[0] == directionRight
+
+		steps := 0
 		for _, b := range v[1:] {
-			if b >= '0' && b <= '9' {
-				steps = steps*10 + int(b-'0')
+			if b < '0' || b > '9' {
+				break
 			}
+			steps = steps*10 + int(b-'0')
 		}
-		var newPosition int
-		// R is 82 in binary
-		if direction == 82 {
-			newPosition = mod((currentPosition + steps), 100)
-		} else {
-			newPosition = mod((currentPosition - steps), 100)
-		}
-		if newPosition == 0 {
-			zeroCounter += 1
-		}
-		// fmt.Println("took", steps, "to", direction, "started", currentPosition, "ended", newPosition)
-		currentPosition = newPosition
+
+		result[i] = instruction{right: right, steps: steps}
 	}
-	return zeroCounter
+	return result
+}
+
+func Part1() any {
+	pos := startPos
+	hits := 0
+	for _, instruction := range instructions {
+		dir, steps := instruction.right, instruction.steps
+		if dir {
+			pos += steps
+		} else {
+			pos -= steps
+		}
+		pos %= dialSize
+		if pos < 0 {
+			pos += dialSize
+		}
+
+		if pos == 0 {
+			hits++
+		}
+	}
+	return hits
 }
 
 func Part2() any {
-	data := util.GetData()
-	var currentPosition = 50
-	var zeroCounter = 0
+	pos := startPos
+	hits := 0
 
-	for _, v := range *data {
-		direction := v[0]
-		var steps = 0
-		for _, b := range v[1:] {
-			if b >= '0' && b <= '9' {
-				steps = steps*10 + int(b-'0')
-			}
-		}
-
-		// R is 82 in binary
-		if direction == 82 {
-			hits := (currentPosition + steps) / 100
-			zeroCounter += hits
-			currentPosition = (currentPosition + steps) % 100
+	for _, ins := range instructions {
+		if ins.right {
+			total := pos + ins.steps
+			hits += total / dialSize
+			pos = total % dialSize
 		} else {
-			target := currentPosition - steps
+			target := pos - ins.steps
 			if target <= 0 {
-				hits := (-target / 100) + 1
-				if currentPosition == 0 {
-					hits -= 1
+				blockHits := (-target / dialSize) + 1
+				if pos == 0 {
+					blockHits--
 				}
-				zeroCounter += hits
+				hits += blockHits
 			}
-			currentPosition = ((currentPosition-steps)%100 + 100) % 100
+
+			pos = target % dialSize
+			if pos < 0 {
+				pos += dialSize
+			}
 		}
 	}
-	return zeroCounter
+
+	return hits
 }
 
 func init() {
 	util.ReadFile("day01/input.txt")
+	instructions = parseInstructions()
 	registry.Register(1, 1, "D01P1", Part1)
 	registry.Register(1, 2, "D01P2", Part2)
 }
