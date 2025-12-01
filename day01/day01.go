965ab9ce6baa07f1448657960ac129e60d2c38ed
Leonhard Breuer <git@leonhardbreuer.de>
2025-12-01 06:32:59 +0100

    day 01: add solutions for part 1 & 2

--- day01/day01.go
+++ day01/day01.go
@@ -6,7 +6,8 @@ package day01
 */
 import "C"
 import (
-	"unsafe"
+	"strconv"
+	"strings"
 
 	"github.com/cheetahbyte/aoc25/registry"
 	"github.com/cheetahbyte/aoc25/util"
@@ -17,23 +18,71 @@ func parseData() []string {
 	return lines
 }
 
+type Operation struct {
+	Direction string
+	Steps     int
+}
+
+func mod(a, b int) int {
+	return ((a % b) + b) % b
+}
+
 func Part1() any {
-	return 0
+	data := parseData()
+	var currentPosition = 50
+	var zeroCounter = 0
+	for _, v := range data {
+		v = strings.TrimSpace(v)
+		if len(v) == 0 {
+			continue
+		}
+		direction := string(v[0])
+		steps, _ := strconv.Atoi(v[1:])
+		var newPosition int
+		if direction == "R" {
+			newPosition = mod((currentPosition + steps), 100)
+		} else {
+			newPosition = mod((currentPosition - steps), 100)
+		}
+		if newPosition == 0 {
+			zeroCounter += 1
+		}
+		// fmt.Println("took", steps, "to", direction, "started", currentPosition, "ended", newPosition)
+		currentPosition = newPosition
+	}
+	return zeroCounter
 }
 
 func Part2() any {
 	data := parseData()
-	cStrArray := make([]*C.char, len(data))
-	for i, s := range data {
-		cStr := C.CString(s)
-		cStrArray[i] = cStr
-		defer C.free(unsafe.Pointer(cStr))
+	var currentPosition = 50
+	var zeroCounter = 0
+
+	for _, v := range data {
+		v = strings.TrimSpace(v)
+		if len(v) == 0 {
+			continue
+		}
+		direction := string(v[0])
+		steps, _ := strconv.Atoi(v[1:])
+
+		if direction == "R" {
+			hits := (currentPosition + steps) / 100
+			zeroCounter += hits
+			currentPosition = (currentPosition + steps) % 100
+		} else {
+			target := currentPosition - steps
+			if target <= 0 {
+				hits := (-target / 100) + 1
+				if currentPosition == 0 {
+					hits -= 1
+				}
+				zeroCounter += hits
+			}
+			currentPosition = ((currentPosition-steps)%100 + 100) % 100
+		}
 	}
-
-	ptrToFirstElem := (**C.char)(unsafe.Pointer(&cStrArray[0]))
-	count := C.int(len(data))
-
-	return C.Part2_Bridge(ptrToFirstElem, count)
+	return zeroCounter
 }
 
 func init() {
