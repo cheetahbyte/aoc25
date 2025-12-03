775a66d74b432f470e4df077276881ccfdb3d911
Leonhard Breuer <git@leonhardbreuer.de>
2025-12-03 17:18:30 +0100

    day 03: use approach from part 2 on part 1

--- day03/day03.go
+++ day03/day03.go
@@ -8,47 +8,17 @@ import (
 	"github.com/cheetahbyte/aoc25/util"
 )
 
-func Part1() any {
-	data := util.GetData()
-	sum := 0
-
-	for _, bank := range *data {
-		maxJoltage := -1
-		for i := 0; i < len(bank)-1; i++ {
-			for j := i + 1; j < len(bank); j++ {
-				d1 := int(bank[i] - '0')
-				d2 := int(bank[j] - '0')
-				currentJoltage := (d1 * 10) + d2
-				if currentJoltage > maxJoltage {
-					maxJoltage = currentJoltage
-				}
-			}
-		}
-		sum += maxJoltage
-	}
-	return sum
-}
-
-func Part2() any {
-	data := util.GetData()
+func GetSum(targetLength int, banks *[]string) int64 {
 	var sum int64 = 0
-	const targetLength = 12
-
-	for _, bank := range *data {
-		if len(bank) < targetLength {
-			continue
-		}
-
+	for _, bank := range *banks {
 		var sb strings.Builder
 		currentPos := 0
 		needed := targetLength
 
 		for needed > 0 {
 			searchEnd := len(bank) - needed
-
 			bestDigit := -1
 			bestIndex := -1
-
 			for i := currentPos; i <= searchEnd; i++ {
 				digit := int(bank[i] - '0')
 
@@ -62,11 +32,22 @@ func Part2() any {
 			currentPos = bestIndex + 1
 			needed--
 		}
-
 		val, _ := strconv.ParseInt(sb.String(), 10, 64)
 		sum += val
 	}
 	return sum
+}
+
+func Part1() any {
+	data := util.GetData()
+	const targetLength = 2
+	return GetSum(targetLength, data)
+}
+
+func Part2() any {
+	data := util.GetData()
+	const targetLength = 12
+	return GetSum(targetLength, data)
 }
 
 func init() {
